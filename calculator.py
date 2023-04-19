e4898516a645618f63339a19a785b5e1ae14985d
Vade-20 <vedantsaxena2012@gmail.com>
2023-04-19 08:00:29 +0530

    .

--- calculator.py
+++ calculator.py
@@ -5,7 +5,11 @@ root.title("Calculator")
 e = Entry(root, fg='red', width=45, bg='black', font=('Times','30'), borderwidth=25, justify=RIGHT, )
 e.grid(row=0, column=0, columnspan=4)
 root.config(bg = 'black')
-root.iconbitmap('Simple-Calculator\\system_application_calculator_13032.ico')
+
+try:
+    root.iconbitmap(r'Simple-Calculator\calculator.ico')
+except:
+    pass
 
 def check(keys): #check where the entered value in the entery box is a valid input if not it prevent the invalid input to be entered
     if str(keys.char).isdigit():
